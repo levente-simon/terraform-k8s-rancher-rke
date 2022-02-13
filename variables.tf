447d1f4c2d444a7199976736ca30bb89662bb40a
levente-simon <ls@leventesimon.com>
2022-02-13 09:03:07 +0100

    set sensitive

--- variables.tf
+++ variables.tf
@@ -1,12 +1,16 @@
 variable "os_user"              { type = string   }
 variable "rancher_url"          { type = string   }
-variable "rancher_token"        { type = string   }
 variable "cluster_name"         { type = string   }
 variable "proxy"                { type = string   }
 variable "no_proxy"             { type = string   }
-variable "ssh_private_key"      { type = string   }
 variable "cluster_hosts"        { type = map(any) }
 variable "metallb_address_pool" { type = string   }
 variable "dns_server"           { type = string }
 variable "dns_port"             { type = string }
 variable "searchdomain"         { type = string }
+variable "rancher_token"        { 
+  type      = string  
+  sensitive = true }
+variable "ssh_private_key"      {
+  type      = string   
+  sensitive = true }
