6be44d2e12b262b8f81a8c18171aee54457cb88b
levente-simon <ls@leventesimon.com>
2022-02-13 18:39:05 +0100

    k8s cluster outputs added

--- outputs.tf
+++ outputs.tf
@@ -11,3 +11,22 @@ output "cluster_kubeconfig" {
   sensitive = true
 }
 
+output "cluster_url" {
+  value     = yamldecode(rancher2_cluster.kube_cluster.kube_config).clusters.0.cluster.server
+  sensitive = true
+}
+
+output "cluster_ca_certificate" {
+  value     = yamldecode(rancher2_cluster.kube_cluster.kube_config).clusters.0.cluster.certificate-authority-data
+  sensitive = true
+}
+
+output "cluster_client_certificate" {
+  value     = yamldecode(rancher2_cluster.kube_cluster.kube_config).users.0.user.client-certificate-data
+  sensitive = true
+}
+
+output "cluster_client_key" {
+  value     = yamldecode(rancher2_cluster.kube_cluster.kube_config).users.0.user.client-key-data
+  sensitive = true
+}
