43530d24a8892e50c68f3013b075783f9740c938
Levente Simon <ls@leventesimon.com>
2022-02-17 23:20:02 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,32 +1,15 @@
 output "cluster_id" {
-  value     = rancher2_cluster.kube_cluster.id
+  depends_on = [ time_sleep.wait_30_sec ]
+  value      = rancher2_cluster.kube_cluster.id
 }
 
 output "cluster_default_project_id" {
-  value     = rancher2_cluster.kube_cluster.default_project_id
+  depends_on = [ time_sleep.wait_30_sec ]
+  value      = rancher2_cluster.kube_cluster.default_project_id
 }
 
 output "cluster_kubeconfig" {
-  value     = base64encode(rancher2_cluster.kube_cluster.kube_config)
-  sensitive = true
+  depends_on = [ time_sleep.wait_30_sec ]
+  value      = base64encode(rancher2_cluster.kube_cluster.kube_config)
+  sensitive  = true
 }
-
-#output "cluster_url" {
-#  value     = yamldecode(rancher2_cluster.kube_cluster.kube_config).clusters.0.cluster.server
-#  sensitive = true
-#}
-#
-#output "cluster_ca_certificate" {
-#  value     = yamldecode(rancher2_cluster.kube_cluster.kube_config).clusters.0.cluster.certificate-authority-data
-#  sensitive = true
-#}
-#
-#output "cluster_client_certificate" {
-#  value     = yamldecode(rancher2_cluster.kube_cluster.kube_config).users.0.user.client-certificate-data
-#  sensitive = true
-#}
-#
-#output "cluster_client_key" {
-#  value     = yamldecode(rancher2_cluster.kube_cluster.kube_config).users.0.user.client-key-data
-#  sensitive = true
-#}
