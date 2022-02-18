3955d6619ff7e2cd2af5f2b30a5c8b858b72e810
levente-simon <ls@leventesimon.com>
2022-02-18 21:19:10 +0100

    chg parameter source to data.rancher...

--- addons.tf
+++ addons.tf
@@ -1,5 +1,5 @@
 resource "time_sleep" "wait_30_seconds" {
-  depends_on = [ rancher2_cluster_sync.sync ]
+  depends_on = [ data.rancher2_cluster.kube_cluster ]
   create_duration = "30s"
 }
 
@@ -11,14 +11,14 @@ resource "time_sleep" "wait_30_seconds" {
 
 resource "rancher2_catalog_v2" "bitnami" {
   depends_on = [ time_sleep.wait_30_seconds ]
-  cluster_id = rancher2_cluster.kube_cluster.id
+  cluster_id = data.rancher2_cluster.kube_cluster.id
   name       = "bitnami"
   url        = "https://charts.bitnami.com/bitnami"
 }
 
 resource "rancher2_catalog_v2" "metallb" {
   depends_on = [ time_sleep.wait_30_seconds ]
-  cluster_id = rancher2_cluster.kube_cluster.id
+  cluster_id = data.rancher2_cluster.kube_cluster.id
   name       = "metallb"
   url        = "https://metallb.github.io/metallb"
 }
@@ -32,7 +32,7 @@ resource "rancher2_catalog_v2" "metallb" {
 resource "rancher2_app_v2" "longhorn" {
   depends_on = [ time_sleep.wait_30_seconds ]
 
-  cluster_id = rancher2_cluster.kube_cluster.id
+  cluster_id = data.rancher2_cluster.kube_cluster.id
   name       = "longhorn"
   repo_name  = "rancher-charts"
   chart_name = "longhorn"
@@ -49,7 +49,7 @@ resource "rancher2_app_v2" "metallb" {
   depends_on = [ rancher2_catalog_v2.metallb,
                  time_sleep.wait_30_seconds ]
 
-  cluster_id = rancher2_cluster.kube_cluster.id
+  cluster_id = data.rancher2_cluster.kube_cluster.id
   name       = "metallb"
   repo_name  = "metallb"
   chart_name = "metallb"
@@ -66,7 +66,7 @@ resource "rancher2_app_v2" "metallb" {
 resource "rancher2_app_v2" "external_dns" {
   depends_on = [ rancher2_catalog_v2.bitnami,
                  time_sleep.wait_30_seconds ]
-  cluster_id = rancher2_cluster.kube_cluster.id
+  cluster_id = data.rancher2_cluster.kube_cluster.id
 
   name       = "external-dns"
   repo_name  = "bitnami"
