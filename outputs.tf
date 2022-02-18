8c33cde830c341387b2b3c20af4fa07a6483c332
Levente Simon <ls@leventesimon.com>
2022-02-18 18:52:44 +0100

    output to come from "data" not resource

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,12 @@
 output "cluster_id" {
-  depends_on = [ time_sleep.wait_30_sec ]
-  value      = rancher2_cluster.kube_cluster.id
+  value      = data.rancher2_cluster.kube_cluster.id
 }
 
 output "cluster_default_project_id" {
-  depends_on = [ time_sleep.wait_30_sec ]
-  value      = rancher2_cluster.kube_cluster.default_project_id
+  value      = data.rancher2_cluster.kube_cluster.default_project_id
 }
 
 output "cluster_kubeconfig" {
-  depends_on = [ time_sleep.wait_30_sec ]
-  value      = base64encode(rancher2_cluster.kube_cluster.kube_config)
+  value      = base64encode(data.rancher2_cluster.kube_cluster.kube_config)
   sensitive  = true
 }
