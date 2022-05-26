7ef70db693bbae6588663bc042ca4f60193b61b7
levente-simon <ls@leventesimon.com>
2022-05-26 19:59:28 +0200

    default proxy

--- variables.tf
+++ variables.tf
@@ -1,13 +1,20 @@
 variable "os_user"              { type = string   }
 variable "rancher_url"          { type = string   }
 variable "cluster_name"         { type = string   }
-variable "proxy"                { type = string   }
-variable "no_proxy"             { type = string   }
 variable "cluster_hosts"        { type = map(any) }
 variable "metallb_address_pool" { type = string   }
 variable "dns_server"           { type = string }
 variable "dns_port"             { type = string }
 variable "searchdomain"         { type = string }
+
+variable "proxy"                {
+  type = string
+  default = ""
+}
+variable "no_proxy"             {
+  type = string
+  default = ""
+}
 variable "rancher_token"        { 
   type      = string  
   sensitive = true
